d8b540a2aae75aa28229f175964244ace3e7088a
dvelopp <dvelopp@gmail.com>
2017-01-05 23:39:48 +0200

    Printer author specified

--- src/tree/TreePrinter.java
+++ src/tree/TreePrinter.java
@@ -3,120 +3,119 @@ package tree;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TreePrinter
-{
+/**
+ * @author The author of the printer is unknown.
+ */
+public class TreePrinter {
 
+    /**
+     * Print a tree
+     *
+     * @param root tree root node
+     */
+    public static void print(Node root) {
+        List<List<String>> lines = new ArrayList<List<String>>();
 
-        /**
-         * Print a tree
-         *
-         * @param root
-         *            tree root node
-         */
-        public static void print(Node root)
-        {
-            List<List<String>> lines = new ArrayList<List<String>>();
+        List<Node> level = new ArrayList<Node>();
+        List<Node> next = new ArrayList<Node>();
 
-            List<Node> level = new ArrayList<Node>();
-            List<Node> next = new ArrayList<Node>();
+        level.add(root);
+        int nn = 1;
 
-            level.add(root);
-            int nn = 1;
+        int widest = 0;
 
-            int widest = 0;
+        while (nn != 0) {
+            List<String> line = new ArrayList<String>();
 
-            while (nn != 0) {
-                List<String> line = new ArrayList<String>();
+            nn = 0;
 
-                nn = 0;
+            for (Node n : level) {
+                if (n == null) {
+                    line.add(null);
 
-                for (Node n : level) {
-                    if (n == null) {
-                        line.add(null);
+                    next.add(null);
+                    next.add(null);
+                } else {
+                    String aa = n.getValue().toString() + " " + n.getColor();
+                    line.add(aa);
+                    if (aa.length() > widest) widest = aa.length();
 
-                        next.add(null);
-                        next.add(null);
-                    } else {
-                        String aa = n.getValue().toString() + " " + n.getColor();
-                        line.add(aa);
-                        if (aa.length() > widest) widest = aa.length();
+                    next.add(n.getLeft());
+                    next.add(n.getRight());
 
-                        next.add(n.getLeft());
-                        next.add(n.getRight());
-
-                        if (n.getLeft() != null) nn++;
-                        if (n.getRight() != null) nn++;
-                    }
+                    if (n.getLeft() != null) nn++;
+                    if (n.getRight() != null) nn++;
                 }
-
-                if (widest % 2 == 1) widest++;
-
-                lines.add(line);
-
-                List<Node> tmp = level;
-                level = next;
-                next = tmp;
-                next.clear();
             }
 
-            int perpiece = lines.get(lines.size() - 1).size() * (widest + 4);
-            for (int i = 0; i < lines.size(); i++) {
-                List<String> line = lines.get(i);
-                int hpw = (int) Math.floor(perpiece / 2f) - 1;
+            if (widest % 2 == 1) widest++;
 
-                if (i > 0) {
-                    for (int j = 0; j < line.size(); j++) {
+            lines.add(line);
 
-                        // split node
-                        char c = ' ';
-                        if (j % 2 == 1) {
-                            if (line.get(j - 1) != null) {
-                                c = (line.get(j) != null) ? '┴' : '┘';
-                            } else {
-                                if (j < line.size() && line.get(j) != null) c = '└';
-                            }
-                        }
-                        System.out.print(c);
+            List<Node> tmp = level;
+            level = next;
+            next = tmp;
+            next.clear();
+        }
 
-                        // lines and spaces
-                        if (line.get(j) == null) {
-                            for (int k = 0; k < perpiece - 1; k++) {
-                                System.out.print(" ");
-                            }
-                        } else {
+        int perpiece = lines.get(lines.size() - 1).size() * (widest + 4);
+        for (int i = 0; i < lines.size(); i++) {
+            List<String> line = lines.get(i);
+            int hpw = (int) Math.floor(perpiece / 2f) - 1;
 
-                            for (int k = 0; k < hpw; k++) {
-                                System.out.print(j % 2 == 0 ? " " : "─");
-                            }
-                            System.out.print(j % 2 == 0 ? "┌" : "┐");
-                            for (int k = 0; k < hpw; k++) {
-                                System.out.print(j % 2 == 0 ? "─" : " ");
-                            }
-                        }
-                    }
-                    System.out.println();
-                }
-
-                // print line of numbers
+            if (i > 0) {
                 for (int j = 0; j < line.size(); j++) {
 
-                    String f = line.get(j);
-                    if (f == null) f = "";
-                    int gap1 = (int) Math.ceil(perpiece / 2f - f.length() / 2f);
-                    int gap2 = (int) Math.floor(perpiece / 2f - f.length() / 2f);
-
-                    // a number
-                    for (int k = 0; k < gap1; k++) {
-                        System.out.print(" ");
+                    // split node
+                    char c = ' ';
+                    if (j % 2 == 1) {
+                        if (line.get(j - 1) != null) {
+                            c = (line.get(j) != null) ? '┴' : '┘';
+                        } else {
+                            if (j < line.size() && line.get(j) != null) c = '└';
+                        }
                     }
-                    System.out.print(f);
-                    for (int k = 0; k < gap2; k++) {
-                        System.out.print(" ");
+                    System.out.print(c);
+
+                    // lines and spaces
+                    if (line.get(j) == null) {
+                        for (int k = 0; k < perpiece - 1; k++) {
+                            System.out.print(" ");
+                        }
+                    } else {
+
+                        for (int k = 0; k < hpw; k++) {
+                            System.out.print(j % 2 == 0 ? " " : "─");
+                        }
+                        System.out.print(j % 2 == 0 ? "┌" : "┐");
+                        for (int k = 0; k < hpw; k++) {
+                            System.out.print(j % 2 == 0 ? "─" : " ");
+                        }
                     }
                 }
                 System.out.println();
-
-                perpiece /= 2;
             }
+
+            // print line of numbers
+            for (int j = 0; j < line.size(); j++) {
+
+                String f = line.get(j);
+                if (f == null) f = "";
+                int gap1 = (int) Math.ceil(perpiece / 2f - f.length() / 2f);
+                int gap2 = (int) Math.floor(perpiece / 2f - f.length() / 2f);
+
+                // a number
+                for (int k = 0; k < gap1; k++) {
+                    System.out.print(" ");
+                }
+                System.out.print(f);
+                for (int k = 0; k < gap2; k++) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+            perpiece /= 2;
         }
     }
+}
